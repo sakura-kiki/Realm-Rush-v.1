e4d6a10502c2c849c5f5d1dd1cf2564bbd99e5db
sakura-kiki <sbilge7631@gmail.com>
2023-05-19 01:02:18 +0300

    Lecture 139: Exploring the World

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour
 {
-    [SerializeField] Nodes currentSearchNode;
-
-    Vector2Int[] directions = {Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down};
+    [SerializeField] Vector2Int startCoordinates;
+    [SerializeField] Vector2Int destinationCoordinates;
+    Nodes startNode;
+    Nodes destinationNode;
+    Nodes currentSearchNode;
+    Queue<Nodes> frontier = new Queue<Nodes>();
+    Dictionary<Vector2Int, Nodes> reached = new Dictionary<Vector2Int, Nodes>();
+    Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
     GridManager gridManager;
-    
-    //Local variable
-    Dictionary<Vector2Int, Nodes> grid;
+
+    //*Local variable
+    Dictionary<Vector2Int, Nodes> grid = new Dictionary<Vector2Int, Nodes>();
 
 
-    void Awake() 
+    void Awake()
     {
         gridManager = FindObjectOfType<GridManager>();
-        if(gridManager != null)
+        if (gridManager != null)
         {
-            //Transferring the public grid to the local one
-            //Without breaking the public grid, we can manipulate the local one
+            //*Transferring the public grid to the local one
+            //*Without breaking the public grid, we can manipulate the local one
             grid = gridManager.Grid;
         }
+        startNode = new Nodes(startCoordinates, true);
+        destinationNode = new Nodes(destinationCoordinates, true);
     }
+
     void Start()
     {
-        ExploreNeighbors();
+        BreadthFirstSearch();
     }
 
     void ExploreNeighbors()
@@ -36,20 +44,46 @@ public class PathFinder : MonoBehaviour
         Debug.Log(neighbors);
 
         Debug.Log(directions);
-        foreach(Vector2Int direction in directions)
+        foreach (Vector2Int direction in directions)
         {
             Debug.Log(direction);
             Vector2Int neighborCoords = currentSearchNode.coordinates + direction;
             Debug.Log(neighborCoords);
 
-            if(grid.ContainsKey(neighborCoords))
+            if (grid.ContainsKey(neighborCoords))
             {
                 neighbors.Add(grid[neighborCoords]);
+            }
 
-                //TODO: Remove after testing
-                grid[neighborCoords].isExplored = true;
-                grid[currentSearchNode.coordinates].isPath = true;
+            foreach (Nodes neighbor in neighbors)
+            {
+                if (!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
+                {
+                    reached.Add(neighbor.coordinates, neighbor);
+                    frontier.Enqueue(neighbor);
+                }
             }
         }
+
+    }
+    void BreadthFirstSearch()
+    {
+        bool isRunning = true;
+
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+
+        while (frontier.Count > 0 && isRunning)
+        {
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if (currentSearchNode.coordinates == destinationCoordinates)
+            {
+                isRunning = false;
+            }
+        }
+
+
     }
 }
